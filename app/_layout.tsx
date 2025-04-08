fbd165fb2f968a463031e9165b71b2d63e6edce1
Gaston Chenet <contact@gastonchenet.com>
2025-04-08 15:58:49 +0200

    Updated router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,33 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
+import { AppMode, getAppMode } from "@/utils/getAppMode";
+import Navbar from "@/components/Navbar";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
+      <Stack
+        screenOptions={{ headerShown: false }}
+        initialRouteName={getAppMode()}
+      >
+        <Stack.Screen name="student" />
+        <Stack.Screen name="teacher" />
       </Stack>
+      <Navbar>
+        <Navbar.Tab
+          icon={<FontAwesome name="user" />}
+          label="Student"
+          href="/student"
+        />
+        <Navbar.Tab
+          icon={<FontAwesome name="user-secret" />}
+          label="Teacher"
+          href="/teacher"
+        />
+      </Navbar>
     </GestureHandlerRootView>
   );
 }
---
+++ app/teacher.tsx
@@ -0,0 +1,9 @@
+import { StatusBar, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+      <Text>Teacher</Text>
+    </View>
+  );
+}
