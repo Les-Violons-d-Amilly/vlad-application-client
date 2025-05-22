0ec722e36551b337eb1f6d3099a95e1db110c368
Marco Orfao <marco.orfao@etu.u-pec.fr>
2025-05-22 15:08:44 +0200

    added : level one reading

--- src/routes/(level)/(reading)/1.tsx
+++ src/routes/(level)/(reading)/1.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function () {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <View style={styles.pageContent}>
+        <Pressable>
+          <Text>Bonjour</Text>
+        </Pressable>
+      </View>
+    </React.Fragment>
+  );
 }
+
+const styles = StyleSheet.create({
+  pageContent: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+});
